ade0b4f3b103f4972b8e72815749ef1a62ea5806
casa <proyectos@lupaweb.com>
2019-01-19 10:47:55 -0500

    subir por primeras vez

---
+++ main.js
@@ -0,0 +1,46 @@
+// ./main.js
+const {app, BrowserWindow} = require('electron')
+
+let win = null;
+
+function createWindow() {
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  const startUrl=process.env.ELECTRON_START_URL || url.format({
+            pathname: path.join(__dirname, build/index.html),
+            protocol: "file",
+            slashes:true
+  });
+  // Specify entry point
+  win.loadURL(startUrl);
+ 
+
+  // Show dev tools
+  // Remove this line before distributing
+  //win.webContents.openDevTools()
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+}
+
+
+app.on('ready', function () {
+
+  createWindow();
+
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
